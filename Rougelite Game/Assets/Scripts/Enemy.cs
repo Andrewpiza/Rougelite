8c2335040d96ffc8634d6decf565e30468523623
Andrewpiza <148090873+Andrewpiza@users.noreply.github.com>
2026-02-03 18:44:37 -0500

    Enemy Sight Range

--- Rougelite Game/Assets/Scripts/Enemy.cs
+++ Rougelite Game/Assets/Scripts/Enemy.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class Enemy : Entity
 {
-    public GameObject target;
+    [SerializeField]private float sightRange = 2;
+    private GameObject target;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     new void Start()
     {
@@ -11,6 +12,21 @@ public class Enemy : Entity
 
     // Update is called once per frame
     void Update()
+    {
+        if (target)FollowTarget();
+        else
+        {
+            foreach (GameObject entity in GameObject.FindGameObjectsWithTag("Entity"))
+            {
+                if (entity.name == "Player" && Vector2.Distance(entity.transform.position,transform.position) < sightRange)
+                {
+                    target = entity;
+                }
+            }
+        }
+    }
+
+    public void FollowTarget()
     {
         Vector2 targetVelocity =target.GetComponent<Rigidbody2D>().linearVelocity.normalized;
         Vector3 targetPos = new Vector3(targetVelocity.x,targetVelocity.y,0) + target.transform.position;
